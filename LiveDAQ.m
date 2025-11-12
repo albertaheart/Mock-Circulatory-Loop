bf4c9f2d63460b97e301ca72ddcbe94189c9dde9
sugoiiia <107055780+sugoiiia@users.noreply.github.com>
2025-11-12 11:30:31 -0700

    Modify LiveDAQ to plot pressure with subplots
    Updated the LiveDAQ script to include subplots for pressure readings instead of voltage. Converted voltage data to pressure using a conversion factor.

--- LiveDAQ.m
+++ LiveDAQ.m
@@ -5,15 +5,28 @@ addinput(dq, "Dev1", "ai1", "Voltage");
 addinput(dq, "Dev1", "ai2", "Voltage");
 dq.Rate = 100;  % samples per second
 
-% --- Create Figure ---
+% --- Create Figure with Subplots ---
 figure;
+
+subplot(3,1,1);  % first subplot
 h0 = animatedline('Color','r','LineWidth',1.5);
-h1 = animatedline('Color','g','LineWidth',1.5);
-h2 = animatedline('Color','b','LineWidth',1.5);
-legend({'ai0','ai1','ai2'});
-title('Live Voltage Readings (ai0–ai2)');
+title('ai0 Pressure');
 xlabel('Time (s)');
-ylabel('Voltage (V)');
+ylabel('Pressure (units)');
+grid on;
+
+subplot(3,1,2);  % second subplot
+h1 = animatedline('Color','g','LineWidth',1.5);
+title('ai1 Pressure');
+xlabel('Time (s)');
+ylabel('Pressure (units)');
+grid on;
+
+subplot(3,1,3);  % third subplot
+h2 = animatedline('Color','b','LineWidth',1.5);
+title('ai2 Pressure');
+xlabel('Time (s)');
+ylabel('Pressure (units)');
 grid on;
 
 % --- Start Timer ---
@@ -23,15 +36,18 @@ startTime = datetime('now');
 while ishandle(h0)
     % Read 0.1 seconds of data
     data = read(dq, seconds(0.1));
+    
+    % Convert voltage to pressure
+    pressureData = data{:,:} * 8.8348;
 
     % Get elapsed time for each sample (a vector)
     numSamples = height(data);
     t = seconds(datetime('now') - startTime) + (0:numSamples-1)'/dq.Rate;
 
-    % Add all samples at once
-    addpoints(h0, t, data{:,1});
-    addpoints(h1, t, data{:,2});
-    addpoints(h2, t, data{:,3});
-
+    % Add all samples at once (pressure instead of voltage)
+    addpoints(h0, t, pressureData(:,1));
+    addpoints(h1, t, pressureData(:,2));
+    addpoints(h2, t, pressureData(:,3));
+    
     drawnow limitrate;
 end
